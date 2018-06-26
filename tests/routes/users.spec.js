97cf175c90cfdc997ccbc602afb7f24ca05fbd35
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-06-26 11:25:24 -0500

    remove old post data

--- tests/routes/users.spec.js
+++ tests/routes/users.spec.js
@@ -3,31 +3,6 @@ var should = require("should");
 
 var app = require("./app");
 
-var test_data = {
-  "users": [
-    {
-      "name": "Bob Dobbs",
-      "username": "bdobbs",
-      "email": "bdobbs@example.com"
-    },
-    {
-      "name": "Sally Jones",
-      "username": "sjones",
-      "email": "sjones@example.com"
-    },
-    {
-      "name": "Adam Stephens",
-      "username": "astephens",
-      "email": "astephens@example.com"
-    },
-    {
-      "name": "Casey Roberts",
-      "username": "croberts",
-      "email": "croberts@example.com"
-    }
-  ]
-}
-
 var user_data = {}
 
 describe('User API Routing', function() {
@@ -52,8 +27,6 @@ describe('User API Routing', function() {
       request(server)
         .get('/')
         .set('Accept', 'application/json')
-        .type('json')
-        .send(test_data)
         .expect(200, check);
     });
   });

df8fafc2a0286d36220af1d938d6cd6a5fd5fd3f
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-06-26 09:33:04 -0500

    add code and tests

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var users = {
+    "users": [
+      {
+        "name": "Bob Dobbs",
+        "username": "bdobbs",
+        "email": "bdobbs@example.com"
+      },
+      {
+        "name": "Sally Jones",
+        "username": "sjones",
+        "email": "sjones@example.com"
+      },
+      {
+        "name": "Adam Stephens",
+        "username": "astephens",
+        "email": "astephens@example.com"
+      },
+      {
+        "name": "Casey Roberts",
+        "username": "croberts",
+        "email": "croberts@example.com"
+      }
+    ]
+  }
+  res.json(users);
 });
 
 module.exports = router;
---
+++ tests/routes/users.spec.js
@@ -0,0 +1,60 @@
+var request = require('supertest');
+var should = require("should");
+
+var app = require("./app");
+
+var test_data = {
+  "users": [
+    {
+      "name": "Bob Dobbs",
+      "username": "bdobbs",
+      "email": "bdobbs@example.com"
+    },
+    {
+      "name": "Sally Jones",
+      "username": "sjones",
+      "email": "sjones@example.com"
+    },
+    {
+      "name": "Adam Stephens",
+      "username": "astephens",
+      "email": "astephens@example.com"
+    },
+    {
+      "name": "Casey Roberts",
+      "username": "croberts",
+      "email": "croberts@example.com"
+    }
+  ]
+}
+
+var user_data = {}
+
+describe('User API Routing', function() {
+  before(function(done) {
+    server = app.listen(done);
+	});
+
+	after(function() {
+		server.close();
+	});
+
+  //Test cases for adding a user
+  describe('Create user API', function() {
+    it('should create user test with a call to GET /', function(done) {
+      function check(err, data) {
+
+        user_data = data.body;
+
+        done(err);
+      }
+
+      request(server)
+        .get('/')
+        .set('Accept', 'application/json')
+        .type('json')
+        .send(test_data)
+        .expect(200, check);
+    });
+  });
+});
